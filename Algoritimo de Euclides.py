193cde88ced4804105c49f77348587d36d3a976a
MatheuSilver <127971044+MatheuSilver@users.noreply.github.com>
2023-03-29 15:38:53 -0300

    Create Algoritimo de Euclides.py

---
+++ Algoritimo de Euclides.py
@@ -0,0 +1,17 @@
+# Dados dois inteiros positivos calcular o máximo divisor comum entre eles usando o algoritmo de Euclides.
+
+
+numa = int(input('escreva um num'))
+numb = int(input('escreva outro num'))
+while numa > 0 and numb > 0:
+    if numa >= numb:
+        numa = numa - numb
+    elif numb > numa:
+        numb = numb - numa
+        
+if numa > 0:
+    print(numa)
+elif numb > 0:
+    print(numb)
+    
+# pequena nota sobre o algoritmo de euclides, em resumo, o MDC pode ser calculado subtraindo o menor número do maior em dado par até o ponto em que algum dos dois seja ZERO
